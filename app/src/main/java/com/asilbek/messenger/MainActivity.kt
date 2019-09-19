893b32fa4282deef5e7a96b1733b55a59faab1f5
ASILBEK AMINOV <thebukharianxo@mail.ru>
2019-09-19 19:17:47 +0500

    First page view

--- app/src/main/java/com/asilbek/messenger/MainActivity.kt
+++ app/src/main/java/com/asilbek/messenger/MainActivity.kt
@@ -3,9 +3,9 @@ package com.asilbek.messenger
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.drawable.AnimationDrawable
+import android.util.Log
 import android.widget.LinearLayout
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +18,20 @@ class MainActivity : AppCompatActivity() {
         animationDrawable.setEnterFadeDuration(4000)
         animationDrawable.setExitFadeDuration(4000)
         animationDrawable.start()
+
+
+
+        RegBtn.setOnClickListener {
+            val email=EmailText.text.toString()
+            val password=PasswordText.text.toString()
+
+            Log.d("MainActivity","Email is: $EmailText")
+            Log.d("MainActivity","Pasword: $PasswordText")
+        }
+
+
+        Already.setOnClickListener {
+            Log.d("MainActivity","Try to show login activity")
+        }
     }
 }
