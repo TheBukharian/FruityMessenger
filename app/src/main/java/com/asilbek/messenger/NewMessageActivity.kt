5bc151318051d737535711233c467aefa4f42e31
ASILBEK AMINOV <thebukharianxo@mail.ru>
2019-10-01 12:05:26 +0500

    bug fixed

--- app/src/main/java/com/asilbek/messenger/NewMessageActivity.kt
+++ app/src/main/java/com/asilbek/messenger/NewMessageActivity.kt
@@ -2,12 +2,20 @@ package com.asilbek.messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+
 supportActionBar?.title="Select User"
+
+
+        recyclerView_newMessage.adapter
+        recyclerView_newMessage.layoutManager=LinearLayoutManager(this)
+
     }
 }
