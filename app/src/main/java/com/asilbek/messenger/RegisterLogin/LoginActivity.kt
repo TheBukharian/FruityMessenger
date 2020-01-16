1f05785917d83f0b628000b15de450a3e4bf1274
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-01-16 18:44:15 +0500

    See Dialog Box Logging in

--- app/src/main/java/com/asilbek/messenger/RegisterLogin/LoginActivity.kt
+++ app/src/main/java/com/asilbek/messenger/RegisterLogin/LoginActivity.kt
@@ -10,16 +10,13 @@ import com.asilbek.messenger.Messages.LatestMessagesActivity
 import com.asilbek.messenger.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
-import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-import kotlinx.android.synthetic.main.custom_toast.*
 
-import kotlinx.android.synthetic.main.custom_toast.view.*
 
 
 class LoginActivity :AppCompatActivity() {
@@ -53,18 +50,16 @@ class LoginActivity :AppCompatActivity() {
 
 
 
-
-
             Log.d("Login", "Attempt login with email/password: $email/****")
-
 
             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener {
 
 
                     if (!it.isSuccessful) return@addOnCompleteListener
-                    //else if successful
-                    var AccName = "Good!"
+
+                    //else if successful>>>
+
 
                     //-------------------Welcome Dialog box---------------------------------
 
@@ -76,15 +71,15 @@ class LoginActivity :AppCompatActivity() {
                     val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
                     ref.addValueEventListener(object : ValueEventListener {
 
+
                             override fun onDataChange(p0: DataSnapshot) {
 
                             currentuserName = p0.getValue(User::class.java)
-
-                                mDialogView.setTitle(currentuserName?.username)
+                                mDialogView.setTitle(""+currentuserName?.username)
 
                                 mDialogView.setIcon(R.mipmap.ic_launcher)
-                                mDialogView.setMessage("Welcome Back!")
-                                mDialogView.setView(welcomeTextView)
+                                mDialogView.setMessage("\t \tWelcome Back!")
+
 
                                 val mAlertD=mDialogView.create()
                                 mAlertD.show()
@@ -104,60 +99,16 @@ class LoginActivity :AppCompatActivity() {
                             Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                         startActivity(intent)
                     }
-
-
-
-
-
-
-
                     Log.d(
                         "Login", "Successfully Signed In user: ${it.result?.user?.uid}"
                     )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//                    //Alert Dialog Builder:
-//                    val mBuilder = AlertDialog.Builder(this)
-//                        .setView(mDialogView)
-//
-//                    //Show Dialog:
-//                    val mAlertDialog = mBuilder.show()
-//
-//                    mDialogView.continueBtn.setOnClickListener {
-//
-//                        mAlertDialog.dismiss()
-//                        val intent = Intent(this, LatestMessagesActivity::class.java)
-//                        intent.flags =
-//                            Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-//                        startActivity(intent)
-//                    }
-
-
                 }
+
+
 
                 .addOnFailureListener {
                     Log.d("Login", "Failed to Sign in User: ${it.message}")
                 }
-
-
         }
-
-
     }
-
-    //set User`s name into Welcome Toast
-
 }
