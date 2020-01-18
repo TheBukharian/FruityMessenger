99764123234f00c15b382bf61e06d29b4f4e3187
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-01-18 18:36:35 +0500

    See Dialog Box Logging in

--- app/src/main/java/com/asilbek/messenger/Messages/LatestMessagesActivity.kt
+++ app/src/main/java/com/asilbek/messenger/Messages/LatestMessagesActivity.kt
@@ -1,11 +1,13 @@
 package com.asilbek.messenger.Messages
 
 import android.content.Intent
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
 import com.asilbek.messenger.R
 import com.asilbek.messenger.RegisterLogin.RegisterActivity
 import com.asilbek.messenger.RegisterLogin.User
@@ -14,6 +16,10 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_latest_messages.*
 
 class LatestMessagesActivity  : AppCompatActivity()  {
 companion object {
@@ -23,8 +29,35 @@ companion object {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
 
+        setupDummyRows()
+
         fetchCurrentUser()
         verifyUserIsLoggedIn()
+
+    }
+
+    class LatestMessageRow: Item<GroupieViewHolder>(){
+        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+        }
+
+        override fun getLayout(): Int {
+
+            return R.layout.latest_messages_row
+        }
+
+
+    }
+
+    private fun setupDummyRows(){
+        val adapter= GroupAdapter<GroupieViewHolder>()
+        recyclerview_latest_messages.adapter=adapter
+
+        adapter.add(LatestMessageRow())
+        adapter.add(LatestMessageRow())
+        adapter.add(LatestMessageRow())
+        adapter.add(LatestMessageRow())
+
 
     }
        private fun verifyUserIsLoggedIn(){
